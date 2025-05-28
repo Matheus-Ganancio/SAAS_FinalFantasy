ad508d0344e5dc4e1a7682746dae3e273bb37e1d
Nyteon-Wol <MatheusEdDominguesGanancio@Gmail.com>
2025-05-27 23:57:36 -0300

    Criação e ajustes do Hero

---
+++ src/app/(panel)/(public)/_components/hero.tsx
@@ -0,0 +1,10 @@
+export function Hero() {
+  return (
+    // Aqui é onde o Hero é estilizado
+    <section className="bg-green-50">
+      <div className="container mx-auto px-4 pt-20">
+
+      </div>
+    </section>
+  );
+}
--- src/app/(panel)/(public)/page.tsx
+++ src/app/(panel)/(public)/page.tsx
@@ -1,10 +1,15 @@
 import { Header } from "./_components/header";
+import { Hero } from './_components/hero'
 
-
-export default function Home(){
+export default function Home() {
   return (
-    <div>
-      <Header/>
+    // Controla o layout layouts
+    <div className="flex flex-col min-h-screen">
+      <Header />
+
+      <div>
+        <Hero />
+      </div>
     </div>
-  )
+  );
 }
