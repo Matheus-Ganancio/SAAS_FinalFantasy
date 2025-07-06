0e1d09a6121cd4e7e8563bf68c3f459d9e36af40
Nyteon-Wol <MatheusEdDominguesGanancio@Gmail.com>
2025-07-05 23:56:21 -0300

    Adicionado prisma

---
+++ src/lib/auth.ts
@@ -0,0 +1,9 @@
+import NextAuth from "next-auth"
+import prisma from "./prisma"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { Adapter } from "next-auth/adapters"
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [],
+})
